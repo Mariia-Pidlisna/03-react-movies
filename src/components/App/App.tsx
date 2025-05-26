c308626ff1a47c5a766482d63713a0926de05073
mariia.pidlisna <mariia.pidlisna@gmail.com>
2025-05-26 18:43:03 +0300

    fix App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -24,6 +24,7 @@ export default function App() {
 
   const handelSearch = async (newQuery: string) => {
     setMoviesSet([]);
+    setSelectedMovie(null);
     setLoading(true);
     setError(false);
     try {
